354ab080233ebbf9802f9775c163fc3347e2fb9a
Daniel <dringler@mail.uni-mannheim.de>
2016-12-08 11:42:10 +0100

    restructure file, add queryPrefix for all KGs

--- createDatasets/src/createDatasetsMain.java
+++ createDatasets/src/createDatasetsMain.java
@@ -22,8 +22,9 @@ public class createDatasetsMain {
         boolean getOptionalP = false;
         boolean secondOrderP = false; //second order can only be specified if getOptionalP is true
 
-        String fileName = "";
-        String secondOrderFileName = "";
+        String fileName;
+        String secondOrderFileName;
+        String header;
 
 
         if (secondOrderP && !getOptionalP) {
@@ -36,7 +37,7 @@ public class createDatasetsMain {
         org.apache.log4j.BasicConfigurator.configure();
         LogManager.getRootLogger().setLevel(Level.OFF); //set console logger off
 
-        String service = "";
+        String service;
         boolean dbIsUp;
         int lineCounter = 0;
         int writtenLines = 0;
@@ -48,66 +49,61 @@ public class createDatasetsMain {
             service = "http://dbpedia.org/sparql";
             dbIsUp = testConnection(service);
             if (dbIsUp) {
-                HashSet<String> dInstances = getEventInstances(k, service);
-                System.out.println(dInstances.size() + " distinct instances received from " + service);
+                //specifiy fileNames and csv header
+                header = "uri\tlabel\tdate\tlat\tlong";
+                if (!getOptionalP) {
+                    fileName = "out/dbpedia-1-basic.tsv";
+                } else {
+                    fileName = "out/dbpedia-2-wOptional.tsv";
+                    header = header + "\tname\tsame\tplace";
+                }
+                secondOrderFileName = "out/dbpedia-3-secondOrder.tsv";
+
+                //get event instances from DBpedia
+                HashSet<String> eventInstances = getEventInstances(k, service);
+                System.out.println(eventInstances.size() + " distinct instances received from " + service);
 
                 try {
                     // 1 get event instances properties
-                    if (!getOptionalP)
-                        fileName = "out/dbpedia-1-basic.tsv";
-                    else
-                        fileName = "out/dbpedia-2-wOptional.tsv";
-                    secondOrderFileName = "out/dbpedia-3-secondOrder.tsv";
                     PrintWriter writer = new PrintWriter(fileName, "UTF-8");
                     //write header
-                    String header = "uri\tlabel\tdate\tlat\tlong";
-                    if (getOptionalP)
-                        header = header + "\tname\tsame\tplace";
-
                     writer.println(header);
 
-                    HashSet<String> dInstancesP = new HashSet<>();
-                    for (String instance : dInstances) {
+                    //get the properties for each event instance
+                    HashSet<String> eventInstanceProperties = new HashSet<>();
+                    for (String instance : eventInstances) {
                         //get all properties for the instance
-                        dInstancesP.addAll(getInstanceProperties(k, service, instance, getOptionalP));
+                        eventInstanceProperties.addAll(getInstanceProperties(k, service, instance, getOptionalP));
                         //write all lines to file
-                        for (String s : dInstancesP) {
-                            //write line to csv
-                            //System.out.println(s);
-                            writer.println(s);
-                        }
-                        writtenLines = writtenLines + dInstancesP.size();
+                        eventInstanceProperties.forEach(writer::println);
+                        writtenLines += eventInstanceProperties.size();
                         //clear set of lines that have been written to file
-                        dInstancesP.clear();
-                        if (lineCounter % lineProgress == 0 && lineCounter != 0)
-                            System.out.println(lineCounter + " instances processed.");
-                        lineCounter += 1;
+                        eventInstanceProperties.clear();
+                        lineCounter = increaseLineCounter(lineCounter, lineProgress);
+
                     }
                     writer.close();
                     System.out.println(writtenLines + " lines written to " + fileName);
+                    //reset counter
                     writtenLines = 0;
                     lineCounter = 0;
 
                     // 2 get second order file
                     if (secondOrderP) {
-                        HashSet<String> dPlaceInstances = getPlaceInstances(k, service);
-                        System.out.println(dPlaceInstances.size() + " distinct place instances received from " + service);
+                        HashSet<String> placeInstances = getPlaceInstances(k, service);
+                        System.out.println(placeInstances.size() + " distinct place instances received from " + service);
                         PrintWriter writer2 = new PrintWriter(secondOrderFileName, "UTF-8");
                         //write header
                         writer2.println("place\tpLat\tpLong\tpSame");
 
-                        HashSet<String> dPlaceInstancesP = new HashSet<>();
-                        for (String placeInstance : dPlaceInstances) {
-                            dPlaceInstancesP.addAll(getPlaceInstanceProperties(k, service, placeInstance));
-
-                            for (String s : dPlaceInstancesP) {
-                                writer2.println(s);
-                            }
-                            writtenLines = writtenLines + dPlaceInstancesP.size();
-                            dPlaceInstancesP.clear();
-                            if (lineCounter % lineProgress == 0 && lineCounter != 0)
-                                System.out.println(lineCounter + " place instances processed.");
-                            lineCounter += 1;
+                        HashSet<String> placeInstancesProperties = new HashSet<>();
+                        for (String placeInstance : placeInstances) {
+                            placeInstancesProperties.addAll(getPlaceInstanceProperties(k, service, placeInstance));
+                            //write lines to file
+                            placeInstancesProperties.forEach(writer2::println);
+                            writtenLines += placeInstancesProperties.size();
+                            placeInstancesProperties.clear();
+                            lineCounter = increaseLineCounter(lineCounter, lineProgress)
 
                         }
                         writer2.close();
@@ -121,16 +117,30 @@ public class createDatasetsMain {
 
         }
         if (yago) {
+            k = 1;
             service = "https://linkeddata1.calcul.u-psud.fr/sparql";
             dbIsUp = testConnection(service);
         }
         if (wikidata) {
+            k = 2;
             service = "https://query.wikidata.org/sparql";
             dbIsUp = testConnection(service);
         }
 
 
     }
+    /**
+     * Increment the lineCounter and print a console log if lineProgress is reached
+     * @param lineProgress
+     * @return incremented lineCounter value
+     */
+    private static int increaseLineCounter(int lineCounter, int lineProgress) {
+        int newLineCounterValue = lineCounter + 1;
+        if (newLineCounterValue % lineProgress == 0)
+            System.out.println(newLineCounterValue + " instances processed.");
+        return newLineCounterValue;
+    }
+
     /**
      * Get all place instance properties. Might (optional) be the following:
      * geo:lat, geo:long, owl:sameAs.
@@ -237,7 +247,6 @@ public class createDatasetsMain {
                     " OPTIONAL { <" + instance + "> foaf:name ?name }\n" +
                     " OPTIONAL { <" + instance + "> owl:sameAs ?same }\n" +
                     " OPTIONAL { <" + instance + "> dbo:place ?place }\n";
-
         }
 
         queryString = queryString +
@@ -246,37 +255,16 @@ public class createDatasetsMain {
         //System.out.println(queryString);
         ResultSet results = queryEndpoint(service, queryString);
 
-        //boolean addSet = false;
         String resultString;
-        String[] properties = {"name", "same",  "place"};
-        //HashSet<String> secondOrderResultStringSet = null;
+        String[] optionalProperties = {"name", "same",  "place"};
         while (results.hasNext()) {
             QuerySolution qs = results.next();
             resultString = instance + "\t" + qs.get("label").toString() + "\t" + qs.get("date").toString() + "\t" + qs.get("lat").toString() + "\t" + qs.get("long").toString();
-           // addSet = false;
             if (getOptionalProperties)
-                resultString = resultString + getAvailableOptionalProperties(qs, properties);
-               /* if(secondOrderP)
-                    //check is qs has an value for the dbo:place property
-                    if (qs.contains("place")) {
-                        addSet = true;
-                        secondOrderResultStringSet = getSecondOrderProperties(service, qs, qs.get("place").toString(), resultString);
-                        //System.out.println(qs.get("place").toString());
-                    } else { //no dbo:place property -> set the 3 values to null
-                        resultString = resultString +  "\tnull\tnull\tnull";
-                    }*/
+                resultString = resultString + getAvailableOptionalProperties(qs, optionalProperties);
 
-
-
-
-            //System.out.println(resultString);
-            /*if (addSet) { //check if multiple strings are returned -> add all to resultSet
-                for (String s : secondOrderResultStringSet) {
-                    resultSet.add(s);
-                }
-            } else { //add single returned string to resultSet*/
             resultSet.add(resultString);
-            //}
+
         }
 
         return resultSet;
@@ -286,12 +274,12 @@ public class createDatasetsMain {
      * Get the optional properties of the QuerySolution.
      * Check if the QuerySolution has the provided properties. Insert "null" otherwise.
      * @param qs
-     * @param properties to check
+     * @param optionalProperties to check
      * @return String (tab-separated with property values or "null" if property value is not available)
      */
-    private static String getAvailableOptionalProperties(QuerySolution qs, String[] properties) {
+    private static String getAvailableOptionalProperties(QuerySolution qs, String[] optionalProperties) {
         String oP = "";
-        for (String p : properties) {
+        for (String p : optionalProperties) {
             //get value for property or add "null" is property is null
             oP = (qs.contains(p)) ? oP + "\t " + qs.get(p).toString() : oP + "\tnull";
         }
@@ -330,22 +318,6 @@ public class createDatasetsMain {
 
         return instanceSet;
     }
-
-    /**
-     * Query the SPARQL endpoint
-     * @param service sparql endpoint URL
-     * @param queryString  query to send to the endpoint
-     * @return ResultSet
-     */
-    private static ResultSet queryEndpoint(String service, String queryString) {
-        Query query = QueryFactory.create(queryString);
-        QueryExecution qe =  QueryExecutionFactory.sparqlService(service, query);
-        ResultSet results = qe.execSelect();
-        ResultSet resultsCopy = ResultSetFactory.copyResults(results);
-        qe.close();
-        return resultsCopy;
-    }
-
     /**
      * Get query prefix
      * @param k 0:DBpedia, 1:YAGO, 2:Wikidata
@@ -363,16 +335,33 @@ public class createDatasetsMain {
                     "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n";
 
         } else if (k==1) {
-            p = "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+            p =     "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
                     "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
                     //"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
                     "PREFIX yago: <http://yago-knowledge.org/resource/>\n"+
-                    //"PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\n" +
-                    // "PREFIX georss: <http://www.georss.org/georss/>\n"+
-                    "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"+
+                    "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n";
+        } else if (k==2) {
+            p =     "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                    //"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
+                    "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+                    "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
                     "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n";
         }
         return p;
+    }
+    /**
+     * Query the SPARQL endpoint
+     * @param service sparql endpoint URL
+     * @param queryString  query to send to the endpoint
+     * @return ResultSet
+     */
+    private static ResultSet queryEndpoint(String service, String queryString) {
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qe =  QueryExecutionFactory.sparqlService(service, query);
+        ResultSet results = qe.execSelect();
+        ResultSet resultsCopy = ResultSetFactory.copyResults(results);
+        qe.close();
+        return resultsCopy;
     }
     /**
      * Test the service connection
