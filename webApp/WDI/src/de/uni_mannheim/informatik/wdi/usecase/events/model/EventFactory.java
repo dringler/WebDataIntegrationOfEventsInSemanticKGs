c3166b65bb0ae1fe643bfd3b83ad6598d637526f
Daniel <dringler@mail.uni-mannheim.de>
2016-12-21 17:59:42 +0100

    process tsv lines

--- webApp/WDI/src/de/uni_mannheim/informatik/wdi/usecase/events/model/EventFactory.java
+++ webApp/WDI/src/de/uni_mannheim/informatik/wdi/usecase/events/model/EventFactory.java
@@ -4,6 +4,9 @@ import de.uni_mannheim.informatik.wdi.model.MatchableFactory;
 import de.uni_mannheim.informatik.wdi.model.Pair;
 import org.w3c.dom.Node;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.HashSet;
 
 /**
  * A {@link MatchableFactory} for {@link Event}s.
@@ -12,6 +15,8 @@ import java.time.LocalDate;
  *
  */
 public class EventFactory extends MatchableFactory<Event> {
+
+    DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
     @Override
     public Event createModelFromElement(Node node, String provenanceInfo) {
         String id = getValueFromChildElement(node, "id");
@@ -55,6 +60,49 @@ public class EventFactory extends MatchableFactory<Event> {
                 Double.valueOf(values[4].substring(0, values[4].indexOf("^")))
         );
         event.addCoordinates(p);
+
+        event.addSame(values[5]);
+        Location location = new Location(values[6], provenanceInfo);
+        event.addLocation(location);
+
+        return event;
+    }
+
+    public Event createModelFromMultpleTSVline(HashSet<String[]> gatheredValues, String provenanceInfo) {
+        Event event = null;
+        boolean firstLine = true;
+        for (String[] values : gatheredValues) {
+            if (firstLine) {
+                event = new Event(values[0], provenanceInfo);
+                firstLine = false;
+            }
+            //fill the attributes
+            //add label after removing the language tag
+            event.addLabel(values[1].substring(0, values[1].indexOf("@")));
+
+
+            // 1214-07-27^^http://www.w3.org/2001/XMLSchema#date
+            //incomplete date 1863-##-##^^http://www.w3.org/2001/XMLSchema#date
+            String date = values[2].substring(0, values[2].indexOf("^")).replace("##", "01");
+            try {
+                event.addDate(LocalDate.parse(date, formatter));
+            } catch (DateTimeParseException e) {
+                //System.out.println(values[0] + " " + date);
+            }
+
+            //50.5833^^http://www.w3.org/2001/XMLSchema#float	3.225^^http://www.w3.org/2001/XMLSchema#float
+            //event.setLat(Double.valueOf(values[3].substring(0, values[3].indexOf("^"))));
+            //event.setLon(Double.valueOf(values[4].substring(0, values[4].indexOf("^"))));
+            Pair<Double, Double> p = new Pair<>(
+                    Double.valueOf(values[3].substring(0, values[3].indexOf("^"))),
+                    Double.valueOf(values[4].substring(0, values[4].indexOf("^")))
+            );
+            event.addCoordinates(p);
+
+            event.addSame(values[5]);
+            Location location = new Location(values[6], provenanceInfo);
+            event.addLocation(location);
+        }
 
         return event;
     }
